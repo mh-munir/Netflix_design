203db93f188100d706c295442c3cec1fae353d46
unknown <mhmunir>
2021-01-30 17:36:55 +0600

    finished all design

--- js/custom.js
+++ js/custom.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
     //toggle the component with class accordion_body
     $(".accordion_head").click(function () {
         if ($('.accordion_body').is(':visible')) {
-            $(".accordion_body").slideUp(100);
-            $(".plusminus").css('transform',' rotate(0deg)');
+            $(".accordion_body").slideUp(300);
+            $(".plusminus").css('transform','rotate(0deg)');
         }
         if ($(this).next(".accordion_body").is(':visible')) {
-            $(this).next(".accordion_body").slideUp(100);
-            $(this).children(".plusminus").css('transform',' rotate(0deg)');
+            $(this).next(".accordion_body").slideUp(300);
+            $(this).children(".plusminus").css('transform','rotate(0deg)');
         } else {
-            $(this).next(".accordion_body").slideDown(100);
-            $(this).children(".plusminus").css('transform',' rotate(45deg)');
+            $(this).next(".accordion_body").slideDown(300);
+            $(this).children(".plusminus").css('transform','rotate(45deg)');
         }
     });
 });
