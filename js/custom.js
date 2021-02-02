40a9a46f3598c5020e01e9dd0c02449a08dc50eb
unknown <mhmunir>
2021-02-02 17:39:51 +0600

    added content

--- js/custom.js
+++ js/custom.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     //toggle the component with class accordion_body
     $(".accordion_head").click(function () {
         if ($('.accordion_body').is(':visible')) {
-            $(".accordion_body").slideUp(300);
+            $(".accordion_body").slideUp(200);
             $(".plusminus").css('transform','rotate(0deg)');
         }
         if ($(this).next(".accordion_body").is(':visible')) {
-            $(this).next(".accordion_body").slideUp(300);
+            $(this).next(".accordion_body").slideUp(200);
             $(this).children(".plusminus").css('transform','rotate(0deg)');
         } else {
-            $(this).next(".accordion_body").slideDown(300);
+            $(this).next(".accordion_body").slideDown(200);
             $(this).children(".plusminus").css('transform','rotate(45deg)');
         }
     });
